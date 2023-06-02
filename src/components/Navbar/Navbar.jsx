99b9aa0e30b2bb7df5c1e6f40529ae60958ee46e
michael <michaeledavi12@gmail.com>
2023-06-02 13:00:23 -0300

    feat: Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import styles from "../../styles/Navbar.module.scss";
+import Link from "next/link";
+import navbarIcon from "../../../public/images/Vector.png";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbarContainer}>
+      <div className={styles.navbar}>
+        <ul>
+          <Link href="#" className={styles.active}>
+            <li>Lorem ipsum</li>
+          </Link>
+          <Link href="#">
+            <li>Lorem ipsum</li>
+          </Link>
+          <Link href="#">
+            <li>Lorem ipsum</li>
+          </Link>
+          <Link href="#">
+            <li>Lorem ipsum</li>
+          </Link>
+          <Link href="#">
+            <li>Lorem ipsum</li>
+          </Link>
+        </ul>
+
+        <div className={styles.search}>
+          <input type="text" placeholder="Buscar" />
+          <Image
+            src={navbarIcon}
+            width={1920}
+            height={76}
+            alt="Header Image"
+            className={styles.searchIcon}
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
